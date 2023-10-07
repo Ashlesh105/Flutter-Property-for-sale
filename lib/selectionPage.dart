a9674478c49c0026f086a979d887a8982cc73d7b
ashlesh <tashleshkini@gmail.com>
2023-10-07 23:40:15 +0530

    day 6

--- lib/selectionPage.dart
+++ lib/selectionPage.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:property_sale/widget.dart';
 
-void main() => runApp(MaterialApp(home: categories(),debugShowCheckedModeBanner: false,));
+void main() => runApp(MaterialApp(
+      home: categories(),
+      debugShowCheckedModeBanner: false,
+    ));
 
 class categories extends StatefulWidget {
   const categories({super.key});
@@ -14,7 +17,9 @@ class _categoriesState extends State<categories> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // drawer:,
       appBar: AppBar(
+
         backgroundColor: Colors.white,
         title: Text(
           'Home',
@@ -22,12 +27,7 @@ class _categoriesState extends State<categories> {
         ),
         centerTitle: true,
         elevation: 0,
-        leading: IconButton(
-            onPressed: () {},
-            icon: Icon(
-              Icons.menu,
-              color: Colors.black,
-            )),
+
         actions: [
           IconButton(
             onPressed: () {},
@@ -36,23 +36,63 @@ class _categoriesState extends State<categories> {
           ),
         ],
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          SizedBox(
-            height: 10,
-          ),
-          Text(
-            'Categories',
-            style: TextStyle(color: Colors.black, fontSize: 25),
-          ),
-          Row(
-            children: [
-              template(
-                  imageUrl: 'assets/home.jfif',text: 'House',),
-            ],
-          )
-        ],
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              'Categories',
+              style: TextStyle(color: Colors.black, fontSize: 25),
+            ),
+            Column(
+              children: [
+                template(
+                    imageUrl: 'assets/home-image.jpg',
+                    text: 'House',
+                    height: MediaQuery.of(context).size.height,
+                    width: MediaQuery.of(context).size.width),
+                template(
+                    imageUrl: 'assets/Appartment.jpg',
+                    text: 'Appartment',
+                    height: MediaQuery.of(context).size.height,
+                    width: MediaQuery.of(context).size.width),
+                template(
+                    imageUrl: 'assets/office.jpg',
+                    text: 'Office',
+                    height: MediaQuery.of(context).size.height,
+                    width: MediaQuery.of(context).size.width),
+                Padding(
+                  padding: EdgeInsets.only(top: 8.0, bottom: 8.0),
+                  child: Align(
+                    alignment: Alignment.bottomCenter,
+                    child: SizedBox(
+                      width: 200,height: 50,
+                      child: OutlinedButton(
+                          onPressed: () {},
+                          child: Text(
+                            'SHOW ALL',
+                            style: TextStyle(color: Colors.black,fontSize: 20.0,fontWeight: FontWeight.w200),
+                            ),
+
+                      style: OutlinedButton.styleFrom(
+                        side: BorderSide(color: Colors.deepPurple),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10.0)
+                        ),
+                        backgroundColor: Colors.white,
+
+                      ),
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
--- lib/widget.dart
+++ lib/widget.dart
@@ -1,26 +1,54 @@
 import 'package:flutter/material.dart';
 
 class template extends StatelessWidget {
-
+  final double height;
+  final double width;
   final String imageUrl;
-   final String text;
-   template({Key?key, required this.imageUrl, required this.text}): super(key: key);
+  final String text;
+  template(
+      {Key? key,
+      required this.imageUrl,
+      required this.text,
+      required this.height,
+      required this.width})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding:  EdgeInsets.all(10.0),
+      padding: EdgeInsets.only(top: 10,bottom: 10,right: 10,left: 10),
       child: Container(
-        height: 200,
-        width: 200,
-        decoration: BoxDecoration(color: Colors.blueGrey,borderRadius: BorderRadius.circular(20.0),boxShadow: []),
-        child: Column(
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(20.0)),
+            boxShadow: [BoxShadow(color: Colors.black12,spreadRadius: 2.0),]),
+        height: height,
+        width: width,
+        child: Stack(
           children: [
-            Image.asset(imageUrl,fit: BoxFit.cover,),
-            SizedBox(height: 36,),
-            Container(height:30,width:200,decoration:BoxDecoration(color: Colors.white),child: Text(text,textAlign: TextAlign.center))
+            ClipRRect(
+                borderRadius: BorderRadius.circular(20.0),
+                child: FittedBox(
+                  child: Image.asset(
+                    imageUrl,
+                    fit: BoxFit.fill,
+                  ),
+                )),
+            Align(
+                alignment: Alignment.bottomCenter,
+                child: Container(
+                    height: 50,
+                    width: width,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.only(bottomLeft: Radius.circular(20.0),bottomRight: Radius.circular(20.0)),
+                      color: Colors.white,
+                    ),
+                    padding: EdgeInsets.symmetric(vertical: 8.0),
+                    child: Text(
+                      text,
+                      textAlign: TextAlign.center,style: TextStyle(fontSize: 20.0,fontWeight: FontWeight.bold),
+                    )))
           ],
-        )
+        ),
       ),
     );
   }
